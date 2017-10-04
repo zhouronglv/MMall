28c62853c4839c420fc60a451fe921f607912a6d
zhourl <zhouronglv@gmail.com>
2017-10-04 20:16:26 +0800

    init spring springmvc mybaits

---
+++ src/main/java/xin/keepmoving/mmall/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package xin.keepmoving.mmall.dao;
+
+import xin.keepmoving.mmall.pojo.Category;
+
+public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
